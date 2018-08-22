450ee83393c2d7180f433cc9666c621c60ec17c6
shvadchak <vitaliy.shvadchak@gmail.com>
2018-08-22 14:10:28 +0300

    Fixed welcome.py

---
+++ functionality/welcome.py
@@ -0,0 +1,8 @@
+from flask_restful import Resource
+# Welcome
+# displays welcome message
+
+
+class Welcome(Resource):
+    def get(self):
+        return 'Hello world, this is our simple REST API Server)'
