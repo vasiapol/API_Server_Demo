9d07d4c82043503a493ac26e25050ca787390938
shvadchak <vitaliy.shvadchak@gmail.com>
2018-08-28 12:08:10 +0300

    Updated

--- functionality/welcome.py
+++ functionality/welcome.py
@@ -1,11 +1,13 @@
 from flask_restful import Resource
 import Prometheus_client
+import time
+
 # Welcome
 # displays welcome message
 
 
 class Welcome(Resource):
+    @REQUEST_TIME.time()
     def get(self):
-        #Prometheus_client.Metrics.inc_c()
         Prometheus_client.Metrics.c.labels('get', '/').inc()
         return 'Hello world, this is our simple REST API Server)'
