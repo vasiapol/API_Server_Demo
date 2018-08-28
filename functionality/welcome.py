035a306f948c38c5ae023da819109c767270f65c
Vitalii Shvadchak <vitaliy.shvadchak@gmail.com>
2018-08-28 10:40:03 +0300

    fixed conflict

--- functionality/welcome.py
+++ functionality/welcome.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from flask_restful import Resource
-# Welcome
-# displays welcome message
-
-
-class Welcome(Resource):
-    def get(self):
-        return 'Hello world, this is our simple REST API Server!'
-=======
 from flask_restful import Resource
 import Prometheus_client
 # Welcome
@@ -16,7 +6,6 @@ import Prometheus_client
 
 class Welcome(Resource):
     def get(self):
-        #Prometheus_client.Metrics.inc_c()
         Prometheus_client.Metrics.c.labels('get', '/').inc()
-        return 'Hello world, this is our simple REST API Server)'
->>>>>>> f_prometheus
+        return 'Hello world, this is our simple REST API Server!'
+
