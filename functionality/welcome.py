1584581c42137f82479106fd99202ee033978a11
Vitalii Shvadchak <vitaliy.shvadchak@gmail.com>
2018-08-29 11:53:42 +0300

    Fixed

--- functionality/welcome.py
+++ functionality/welcome.py
@@ -11,11 +11,4 @@ class Welcome(Resource):
 
     def get(self):
         Prometheus_client.Metrics.welcome_page_request.inc()
-        html_str="""<html>
-<header><title>This is title</title></header>
-<body>
-Hello world
-</body>
-</html>"""
-        # return 'Hello world, this is our simple REST API Server!'
-         return html_str
+        return 'Hello world, this is our simple REST API Server!'
