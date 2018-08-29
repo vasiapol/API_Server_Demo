7dbba4c7826ce5605b2c42fc10d66175349bb00d
Vitalii Shvadchak <vitaliy.shvadchak@gmail.com>
2018-08-29 12:49:59 +0300

    Updated

--- API.py
+++ API.py
@@ -20,4 +20,4 @@ api.add_resource(welcome.Welcome, '/')
 if __name__ == "__main__":
     Prometheus_client.Metrics.start()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
