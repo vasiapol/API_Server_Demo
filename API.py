fdd8efaed8428e70e69fa52038addfd55329dab2
shvadchak <vitaliy.shvadchak@gmail.com>
2018-09-24 22:09:54 +0300

    wsgi

--- API.py
+++ API.py
@@ -16,5 +16,4 @@ api.add_resource(welcome.Welcome, '/')
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
