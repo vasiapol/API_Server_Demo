628d8e81a0f5bbb7cb48ab265287e22cfcdd114e
Vitalii Shvadchak <vitaliy.shvadchak@gmail.com>
2018-10-08 16:42:28 +0300

    Update 'make_kubernetes.mk'

--- make_kubernetes.mk
+++ make_kubernetes.mk
@@ -1,26 +1,31 @@
-uniq:=$(shell uuidgen)
-tag = $(shell whoami)-dev-$(uniq)
+    uniq:= $(shell uuidgen)
+    tag = $(shell whoami)-dev-$(uniq)
 
-.PHONY: k8s_deploy
-k8s_deploy: docker_image_build registry_push build_manifest kube_deploy
+    .PHONY: k8s_deploy
+    k8s_deploy: docker_image_build registry_push build_manifest kube_deploy rebuild_example_manifest
 
-.PHONY: docker_image_build
-docker_image_build:
-		docker build --tag $(name) .
-        docker tag $(name):latest polianskiyvasyl/$(name):$(tag)
+    .PHONY: docker_image_build
+    docker_image_build:
+    	docker build --tag $(name) .
+    	docker tag $(name):latest polianskiyvasyl/$(project)-$(name):$(tag)
 
-.PHONY: build_manifest
-build_manifest:
-        sed -i "s|__IMAGE__|polianskiyvasyl/$(name):$(tag)|g" k8s_API.yml
-        sed -i "s|__NAME__|$(name)|g"  k8s_API.yml
+    .PHONY: build_manifest
+    build_manifest:
+    	sed -i "s|__IMAGE__|polianskiyvasyl/$(project)-$(name):$(tag)|g" API_example.yml
+    	sed -i "s|__NAME__|$(name)|g"  API_example.yml
 
-.PHONY: registry_push
-registry_push:
-        docker push polianskiyvasyl/$(name):$(tag)
+    .PHONY: registry_push
+    registry_push:
+    	docker push polianskiyvasyl/$(project)-$(name):$(tag)
 
-.PHONY: kube_deploy
-kube_deploy:
-        kubectl apply -f k8s_API.yml
+    .PHONY: kube_deploy
+    kube_deploy:
+    	kubectl apply -f API_example.yml
+
+    .PHONY: rebuild_example_manifest
+    rebuild_example_manifest:
+    	sed -i "s|polianskiyvasyl/$(project)-$(name):$(tag)|__IMAGE__|g" API_example.yml
+    	sed -i "s| $(name)| __NAME__|g"  API_example.yml
 
 ~
 ~
