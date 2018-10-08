852d8c24d48a487e35fc835a669b160030c340bb
Vitalii Shvadchak <vitaliy.shvadchak@gmail.com>
2018-10-08 16:54:51 +0300

    Update 'make_kubernetes.mk'

--- make_kubernetes.mk
+++ make_kubernetes.mk
@@ -6,26 +6,26 @@ k8s_deploy: docker_image_build registry_push build_manifest kube_deploy rebuild_
 
 .PHONY: docker_image_build
 docker_image_build:
-   	docker build --tag $(name) .
-   	docker tag $(name):latest polianskiyvasyl/$(project)-$(name):$(tag)
+	docker build --tag $(name) .
+	docker tag $(name):latest polianskiyvasyl/$(project)-$(name):$(tag)
 
 .PHONY: build_manifest
 build_manifest:
-   	sed -i "s|__IMAGE__|polianskiyvasyl/$(project)-$(name):$(tag)|g" API_example.yml
-   	sed -i "s|__NAME__|$(name)|g"  API_example.yml
+	sed -i "s|__IMAGE__|polianskiyvasyl/$(project)-$(name):$(tag)|g" API_example.yml
+	sed -i "s|__NAME__|$(name)|g"  API_example.yml
 
 .PHONY: registry_push
 registry_push:
-   	docker push polianskiyvasyl/$(project)-$(name):$(tag)
+	docker push polianskiyvasyl/$(project)-$(name):$(tag)
 
 .PHONY: kube_deploy
 kube_deploy:
-   	kubectl apply -f API_example.yml
+	kubectl apply -f API_example.yml
 
 .PHONY: rebuild_example_manifest
 rebuild_example_manifest:
-   	sed -i "s|polianskiyvasyl/$(project)-$(name):$(tag)|__IMAGE__|g" API_example.yml
-   	sed -i "s| $(name)| __NAME__|g"  API_example.yml
+	sed -i "s|polianskiyvasyl/$(project)-$(name):$(tag)|__IMAGE__|g" API_example.yml
+	sed -i "s| $(name)| __NAME__|g"  API_example.yml
 
 ~
 ~
