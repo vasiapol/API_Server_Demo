46878cdfb1540418056f84c8b236981499012bc7
shvadchak <vitaliy.shvadchak@gmail.com>
2018-10-09 15:06:00 +0300

    Modified

--- curl_rest.sh
+++ curl_rest.sh
@@ -1,9 +1,10 @@
-
-curl http://192.168.0.100:5000/
-curl http://192.168.0.100:5000/api/v1/trainees
-curl http://192.168.0.100:5000/api/v1/trainees/<id>
-curl -X DELETE http://192.168.0.100:5000/api/v1/trainees/<id>
-curl -X POST -d "Firstname=test&Lastname=test&Age=10" http://192.168.0.100:5000/api/v1/trainees
-curl -X PUT -d Firstname=test http://192.168.0.100:5000/api/v1/trainees/<id>
+curl -k https://192.168.103.240/
+curl -k https://192.168.103.240/api/v1/trainees
+curl -k https://192.168.103.240/api/v1/trainees/5
+curl -k -X POST -d "Firstname=Demo1&Lastname=Demo1&Age=10&Married=NO" https://192.168.103.240/api/v1/trainees
+curl -k -X PATCH -d Firstname=test https://192.168.103.240/api/v1/trainees/
+curl -k https://192.168.103.240/api/v1/trainees
+curl -k -X DELETE https://192.168.103.240/api/v1/trainees/29
+curl -k https://192.168.103.240/api/v1/trainees
 while true;do mysql -u haproxy_check -h 192.168.103.240 -P 32596 -e 'select @@hostname;'|grep galera && sleep 1;done
-while true;do curl --insecure https://192.168.103.240/ && sleep 1;done
+while true;do curl -k https://192.168.103.240/ && sleep 1;done
