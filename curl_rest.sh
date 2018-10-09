e7254b4e1bcb2a4d2ea1fb6ebdd2daeb688e4fac
vasa <poliabchuk94@gmail.com>
2018-10-09 14:05:45 +0300

    Update 'curl_rest.sh'

--- curl_rest.sh
+++ curl_rest.sh
@@ -5,3 +5,5 @@ curl http://192.168.0.100:5000/api/v1/trainees/<id>
 curl -X DELETE http://192.168.0.100:5000/api/v1/trainees/<id>
 curl -X POST -d "Firstname=test&Lastname=test&Age=10" http://192.168.0.100:5000/api/v1/trainees
 curl -X PUT -d Firstname=test http://192.168.0.100:5000/api/v1/trainees/<id>
+while true;do mysql -u haproxy_check -h 192.168.103.240 -P 32596 -e 'select @@hostname;'|grep galera && sleep 1;done
+while true;do curl --insecure https://192.168.103.240/ && sleep 1;done
