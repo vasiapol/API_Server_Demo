ae7b3ff20548fa7d9e601b42e6437847ff0aa54b
Vitalii Shvadchak <vitaliy.shvadchak@gmail.com>
2018-10-10 12:10:16 +0300

    Update 'sql/FillTheTable.sql'

--- sql/FillTheTable.sql
+++ sql/FillTheTable.sql
@@ -2,5 +2,5 @@ INSERT INTO TraineeList(Firstname, Lastname, Age, Married)
 VALUES('Vitalii', 'Shvadchak', 22,'No'),
 ('Vasyl', 'Polianskii',24,'Yes'),
 ('Bohdan', 'Glibovitskiy',32,'Yes'),
-('Mykola', 'Semenovych',30,'No')
+('Mykola', 'Semenovych',34,'No')
 ;
